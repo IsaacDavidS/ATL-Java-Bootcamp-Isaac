262a08e36c632fccd734eb5ee4fcb93757aa953e
IsaacDavidS <isaacdsolisb@gmail.com>
2023-06-06 16:10:23 -0500

    Adding changes

--- EjerciciosClase5/AdivinaElNumero.java
+++ EjerciciosClase5/AdivinaElNumero.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class AdivinaElNumero {
     public static void main(String[] args) {
 
-        double numeroRandom = Math.random() * 10;
+        double numeroRandom = Math.random() * 100;
+
 
         //Le pedimos al usuario que adivine el número
 
         System.out.println("Ingresa un número del 1 al 100: ");
         Scanner sc = new Scanner(System.in);
         double numero = sc.nextDouble();
+
+        System.out.println(numeroRandom);
 
         if (numero == numeroRandom){
             System.out.println("El número es correcto. Acertaste");
